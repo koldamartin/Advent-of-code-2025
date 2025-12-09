55b9afe859448cf37e4a5cffdfe1ea79be33ece0
Martin Kolda <mkko@seznam.cz>
2025-12-09 13:26:11 +0100

    day 5 part 2

--- day5/day5.js
+++ day5/day5.js
@@ -13,6 +13,8 @@ const inputArray = readFileToArray();
 ranges_input = inputArray[0].split('\n');
 numbers_input = inputArray[1].split('\n');
 
+
+// part A
 let resultA = 0;
 
 for (const number of numbers_input) {
@@ -26,3 +28,38 @@ for (const number of numbers_input) {
     }
 }
 console.log('resultA:', resultA);
+
+
+// part B
+
+let ranges = ranges_input.map(range => range.split('-').map(Number));
+// sort ranges from lowest to highest by the 0 index
+ranges.sort((a, b) => a[0] - b[0]);
+
+// calculate absolute minimum and maximum from ranges
+let absolutMin = Math.min(...ranges.map(range => range[0]));
+let absolutMax = Math.max(...ranges.map(range => range[1]));
+
+let totalRange = absolutMax - absolutMin + 1;
+let continuousMax = 0
+
+for (let i = 0; i < ranges.length; i++) {
+    // do not iterate over last element
+    if (i === ranges.length - 1) {
+        break;
+    }
+    let currentMax = ranges[i][1];
+    if (currentMax > continuousMax) {
+        continuousMax = currentMax;
+    }
+
+    if (ranges[i + 1][0] > ranges[i][1] && ranges[i + 1][0] > continuousMax) {
+
+        let rottenIngredients = ranges[i + 1][0] - continuousMax - 1;
+        console.log('rottenIngredients between ranges ' + ranges[i] + ' and ' + ranges[i + 1] + ':', rottenIngredients);
+        totalRange -= rottenIngredients;
+    }
+}
+
+let solutionB = totalRange;
+console.log('solutionB:', solutionB);
